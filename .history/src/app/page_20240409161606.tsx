7238570be5cdc53966debcbaa2a8a7a018388dbc
MikeeBuilds <citizensofandrometta@gmail.com>
2024-04-09 17:21:50 -0400

    taking a break

---
+++ .history/src/app/page_20240409161606.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Navbar from "../components/global/navbar";
+
+export default function Home() {
+  return (
+    <main>
+      <Navbar/>
+      <section className="h-screen w-full bg-neutral-950 rounded-md !overflow-visible relative flex flex-col items-center 
+      antialiased">
+        <div className="absolute inset-0 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%, #000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[]"></div>
+      </section>
+    </main>
+  );
+}
